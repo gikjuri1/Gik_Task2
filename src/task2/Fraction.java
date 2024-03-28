b347489181efafe67a0d379593ef090ae88e0ab3
gikjuri1 <gikjuri@yandex.ru>
2024-03-28 16:36:49 +0300

    Made all for cache in proxy

--- src/task2/Fraction.java
+++ src/task2/Fraction.java
@@ -5,40 +5,26 @@ import java.lang.reflect.Proxy;
 public class Fraction implements Fractionable {
     private int num;
     private int denum;
-    public double cache;
+
     public Fraction (int num, int denum) {
         this.num=num;
         this.denum=denum;
-        this.cache= 0;
     }
     @Mutator
     public void setNum(int num) {
         this.num = num;
-        System.out.println("erase cache");
-        this.cache=0;
     }
 
     @Mutator
     public void setDenum(int denum){
         this.denum=denum;
-        System.out.println("erase cache");
-        this.cache=0;
     }
-
-    public int getNum() {
-        return num;
-    }
-
-    public int getDenum() {
-        return denum;
-    }
-
     @Override
     @Cache
     public double doubleValue() {
-        this.cache= num/denum;
-        System.out.println("invoke double value, cache=" + this.cache);
-        return this.cache;
+        double res= num/denum;
+        System.out.println("invoke double value");
+        return res;
     }
     public  Object getProxy()
     {
--- src/task2/FractionableInvHandler.java
+++ src/task2/FractionableInvHandler.java
@@ -1,13 +1,13 @@
 package task2;
 
 import java.lang.annotation.Annotation;
-import java.lang.reflect.Field;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
-import java.util.Arrays;
 
 public class FractionableInvHandler implements InvocationHandler {
     private Object obj;
+    private int modCount=1;
+    private Object cachedVal;
 
     FractionableInvHandler(Object obj) {
         this.obj = obj;
@@ -19,23 +19,28 @@ public class FractionableInvHandler implements InvocationHandler {
         Method m = obj.getClass().getMethod(method.getName(), method.getParameterTypes());
 
         Annotation[] anns = m.getDeclaredAnnotations();
-        /*Annotation[] anns = m.getAnnotationsByType(JustForFun.class);
-        for (Annotation a: anns)
-        {
-            System.out.println("Ha ha ha ha ha ha ha ha ha");
-        }*/
-        if (Arrays.stream(anns).filter(x->((Annotation)x).annotationType().equals(Cache.class)).count()>0) {
-            //Берем значение из кэша
-            Field cacheField = obj.getClass().getDeclaredField("cache");
-            double cache=cacheField.getDouble(obj);
-
-            if (cache==0) {
-                return method.invoke(obj, args);
+        if (m.isAnnotationPresent(Cache.class)) {
+            if (modCount>0){
+                //Вызываем метод
+                System.out.println("Not cached val:");
+                cachedVal = method.invoke(obj, args);
+                //Обнуляем счетчик кэша
+                modCount=0;
+                return cachedVal;
             }
-            else {
-                return cache;
+            else{
+                //Берем значение из кэша
+                System.out.println("Cached val:");
+                return cachedVal;
             }
         }
+        if (m.isAnnotationPresent(Mutator.class)) {
+            //Увеличиваем счетчик кэша, вызываем метод
+            modCount++;
+            System.out.println("Not cached val from mutator");
+            return method.invoke(obj, args);
+        }
+
         return method.invoke(obj, args);
     }
 
